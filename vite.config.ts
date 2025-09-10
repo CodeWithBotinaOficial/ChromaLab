b585fc9f1626c88965558e325bc9cafb7d873ce0
Diego Botina <code.with.botina@gmail.com>
2025-09-10 18:15:00 -0500

    Feat: Prepare for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/ChromaLab/", // Add this line for GitHub Pages deployment
   resolve: {
     alias: [
       { find: '@', replacement: '/src' }
